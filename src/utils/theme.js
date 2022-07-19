6bd279664f4901bf37ce49f872ab11f9f8d4d564
Darek Mazur <dev@darekmazur.it>
2022-07-19 15:08:19 +0200

    create style theme

---
+++ src/utils/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    white: "#fff",
+    grey: "#EBECEC",
+    blue: "#235489",
+    red: "#B82F2E",
+    yellow: "#D1A92B",
+    black: "#000",
+  },
+  fonts: {
+    mainFont: '"Montserrat", sans-serif',
+    secondaryHeaderFont: '"Noto Serif Display", serif',
+    headerFont: '"Playfair Display SC", serif',
+  },
+  fontSize: {
+    xxl: "5rem",
+    xl: "4.2rem",
+    l: "3rem",
+    lm: "1.8rem",
+    m: "1.6rem",
+    s: "1.4rem",
+  },
+};
