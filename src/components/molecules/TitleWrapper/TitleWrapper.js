83e83a851811605b808d24cfaf3ab7d6c22b11c7
Darek Mazur <dev@darekmazur.it>
2022-07-21 00:09:41 +0200

    animate title

--- src/components/molecules/TitleWrapper/TitleWrapper.js
+++ src/components/molecules/TitleWrapper/TitleWrapper.js
@@ -1,17 +1,40 @@
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
 import Button from "../../atoms/Button/Button";
 import Title from "../../atoms/Title/Title";
 import TitleIcon from "../../atoms/TitleIcon/TitleIcon";
+import { TitleContent } from "../TitleContent/TitleContent.styles";
 import { StyledTitleWrapper } from "./TitleWrapper.styles";
 
 const TitleWrapper = ({ handleClick, isOpen }) => {
+  const titleRef = useRef();
+
+  useEffect(() => {
+    gsap.fromTo(
+      titleRef.current.children,
+      { x: "-=150%", autoAlpha: 0 },
+      {
+        x: 0,
+        autoAlpha: 1,
+        duration: 1,
+        ease: "power3",
+        stagger: 0.1,
+        scrollTrigger: {
+          trigger: titleRef.current,
+          start: "top center+=100px",
+        },
+      }
+    );
+  }, []);
+
   return (
     <>
       <StyledTitleWrapper>
         <TitleIcon />
-        <div>
+        <TitleContent ref={titleRef}>
           <Title title="System nieodpłatnej pomocy" color="red" />
           <Title title="wszystko, co musisz wiedzieć" isSubtitle />
-        </div>
+        </TitleContent>
       </StyledTitleWrapper>
       <Button
         label={isOpen ? "Zamknij" : "Dowiedz się wiecej"}
