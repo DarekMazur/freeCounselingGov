c59038bf80bf01901840afe2f6b6245d753b8580
Darek Mazur <dev@darekmazur.it>
2022-07-21 16:38:11 +0200

    style form view

--- src/views/FormView/FormView.js
+++ src/views/FormView/FormView.js
@@ -3,10 +3,20 @@ import { Container } from "../../components/atoms/Container/Container.styles";
 import Title from "../../components/atoms/Title/Title";
 import TitleWrapper from "../../components/molecules/TitleWrapper/TitleWrapper";
 import { Wrapper } from "../../components/molecules/Wrapper/Wrapper.styles";
+import { useContext, useEffect } from "react";
 import QuestionItem from "../../components/organisms/QuestionItem/QuestionItem";
 import { questions } from "../../data/questions";
+import { ContentContext } from "../../providers/ContentProvider";
+import { FormWrapper } from "../../components/organisms/FormWrapper/FormWrapper.styles";
 
 const FormView = () => {
+  const { reset } = useContext(ContentContext);
+
+  useEffect(() => {
+    reset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container padding="3rem">
       <TitleWrapper
@@ -20,7 +30,7 @@ const FormView = () => {
           isSubtitle
           isLeft
         />
-        <div>
+        <FormWrapper>
           {questions.map((question) => (
             <QuestionItem
               key={question.id}
@@ -30,8 +40,8 @@ const FormView = () => {
               value={question.value}
             />
           ))}
-        </div>
-        <Button label="Wyślij" />
+          <Button label="Wyślij" />
+        </FormWrapper>
       </Wrapper>
     </Container>
   );
