3eeb60f345d59c997b1050e7dc7b4e4955eceb01
Darek Mazur <dev@darekmazur.it>
2022-08-02 14:42:22 +0200

    fix misspelling

--- src/views/ResultsView/ResultsView.js
+++ src/views/ResultsView/ResultsView.js
@@ -1,12 +1,12 @@
-import ResultsWrapper from '../../../components/ResultsWrapper/ResultsWrapper';
-import npp from '../../../assets/images/npp.jpg';
-import npo from '../../../assets/images/npo.jpg';
-import nm from '../../../assets/images/nm.jpg';
-import unknown from '../../../assets/images/unknown.jpg';
+import ResultsWrapper from "../../../components/ResultsWrapper/ResultsWrapper";
+import npp from "../../../assets/images/npp.jpg";
+import npo from "../../../assets/images/npo.jpg";
+import nm from "../../../assets/images/nm.jpg";
+import unknown from "../../../assets/images/unknown.jpg";
 
-import { useContext } from 'react';
-import ContentPovider from '../../providers/ContentProvider';
-import { faq } from '../../data/faq';
+import { useContext } from "react";
+import ContentPovider from "../../providers/ContentProvider";
+import { faq } from "../../data/faq";
 
 const ResultsView = () => {
   const { globalCount, globalValue } = useContext(ContentPovider);
@@ -20,22 +20,23 @@ const ResultsView = () => {
 
   if (output === 10 / 8 || globalCount === 18) {
     resultObj.image = npp;
-    resultObj.title = 'Nieodpłatna Pomoc Prawna';
+    resultObj.title = "Nieodpłatna Pomoc Prawna";
     resultObj.description = faq[0].answer;
   } else if (output === 10 / 6) {
     resultObj.image = npo;
-    resultObj.title = 'Nieodpłatne Poradnictwo Obywatelskie';
+    resultObj.title = "Nieodpłatne Poradnictwo Obywatelskie";
     resultObj.description = faq[1].answer;
   } else if (output === 10 / 4) {
     resultObj.image = nm;
-    resultObj.title = 'Nieodpłatne Mediacje';
+    resultObj.title = "Nieodpłatna Mediacja";
     resultObj.description = faq[2].answer;
   }
 
   return (
     <>
+      {console.log(resultObj)}
       {globalValue !== 0 ? (
-        <ResultsWrapper>
+        <ResultsWrapper
           image={resultObj.image}
           result={resultObj.title}
           description={resultObj.description}
